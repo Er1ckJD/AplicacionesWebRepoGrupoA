97c4d1320f53a2b9e304ff45366baa9fb9e85520
Erick Jimenez Dominguez <201020117@ucc.mx>
2025-05-01 12:26:39 -0600

    trabajos

--- mongodb/servercrud/index.js
+++ mongodb/servercrud/index.js
@@ -1,31 +1,32 @@
-const express = reuire ('express');
-const cors = require ('cors');
-const app = express();
-const port =3000;
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
 
+const app = express();
+const port = 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-const{MongoClient} = requie('mongodb');
+const uri = 'mongodb+srv://Erick:@EricK2016@cluster0.uhv0m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const client = new MongoClient(uri);
 
-const uri = 'mongodb+srv://202260138:aMigoS6A@baloo.1nlyg.mongodb.net/?retryWrites=true&w=majority&appName=Baloo'
-const cliente = new MongoClient(uri);
-
-app.post('/insertar', async(req,res)=>{
-    try{
-        await cliente.connect();
-        const db=cliente.db(DataBaseGrupoA);
-        const coleccion=db.coleccion('users');
-        await coleccion.insertOne({usuario:'pepepassword:'pepe'});
-        req.send ("Documento agregado exitosamente");
-    }finally{
-        await cliente.close();
+app.post('/insertar', async (req, res) => {
+    try {
+        await client.connect();
+        const db = client.db('DataBaseGrupoA');
+        const collection = db.collection('users');
+        await collection.insertOne({ usuario: 'pepe', password: 'pepe' });
+        res.send("Documento agregado exitosamente");
+    } catch (error) {
+        console.error("Error connecting to MongoDB or inserting document:", error);
+        res.status(500).send("Error al agregar el documento");
+    } finally {
+        await client.close();
     }
 });
 
-app.listen(port,()=>{
-
-    console.log(server listenig at http:localhost//localhost:${port});
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
 });
